fd2311229def39bef72253c2850edb637e44b356
Muhammad Nizamuddin Aulia <muhammadnizamuddinaulia@gmail.com>
2023-09-27 23:30:05 +0700

    Rename app name

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,5 @@
 Apipie.configure do |config|
-  config.app_name                = "Nizam Backend"
+  config.app_name                = "Rails Promotion"
   config.copyright               = "Muhammad Nizamuddin Aulia"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/api/docs"
